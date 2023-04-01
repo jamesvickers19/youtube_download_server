2870c35492b3efd2cb2dd9e062e4c4a9ff47cb3a
James Vickers <jamesvickers19@gmail.com>
2023-04-01 13:07:11 -0600

    Fix build fronend script for when directory already exists

--- build_frontend.sh
+++ build_frontend.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # do in subshell so it doesn't change terminal directory
-(cd frontend && yarn build && mv build ../server/app/static)
+(cd frontend && yarn build && rm -r ../server/app/static && mv build ../server/app/static)
