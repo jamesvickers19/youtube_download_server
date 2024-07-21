75ce7a84a872ca848442e3969fa8c848e6785d62
James Vickers <jamesvickers19@gmail.com>
2024-07-21 11:42:27 -0600

    fix folder copy path

--- push_server.sh
+++ push_server.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-bash build_frontend.sh
+# bash build_frontend.sh
 
 ADDRESS=142.93.8.222
 
 scp -r nginx root@${ADDRESS}:/youtube-downloader/
 
-scp -r server/app root@${ADDRESS}:/youtube-downloader/server/app
+scp -r server/app/* root@${ADDRESS}:/youtube-downloader/server/app
 
 scp .env docker-compose.yml redeploy_server.sh init-letsencrypt.sh root@${ADDRESS}:/youtube-downloader/
 
