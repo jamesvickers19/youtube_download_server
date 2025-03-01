8cf085aaad2f5db067d72bb86494e2e52c91137a
James Vickers <jamesvickers19@yahoo.com>
2025-03-01 11:43:48 -0700

    include new file cleaner files in upload script

--- push_server.sh
+++ push_server.sh
@@ -10,6 +10,10 @@ scp -r nginx root@${ADDRESS}:/youtube-downloader/
 
 scp -r server/app/* root@${ADDRESS}:/youtube-downloader/server/app
 
+scp server/cleanup_download_files.sh root@${ADDRESS}:/youtube-downloader/server
+
+scp server/Dockerfile.file_cleaner root@${ADDRESS}:/youtube-downloader/server
+
 scp .env docker-compose.yml redeploy_server.sh init-letsencrypt.sh root@${ADDRESS}:/youtube-downloader/
 
 scp server/Dockerfile server/requirements.txt root@${ADDRESS}:/youtube-downloader/server
